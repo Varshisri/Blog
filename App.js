21945977a69c785a56716e701b227b25bcfe3167
Varshisri <43078017+Varshisri@users.noreply.github.com>
2020-05-28 12:21:39 -0400

    adding changes like provider and redux from scratch we built instead of using redux

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer } from 'react-navigation';
 import IndexScreen from './src/screens/IndexScreen';
 import React from 'react';
-import { BlogProvider} from './src/context/BlogContext';
+import { Provider as BlogProvider} from './src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,4 @@
-import React, { useReducer } from 'react';
-import { add } from 'react-native-reanimated';
-
-const BlogContext = React.createContext();
+import createDataCotext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch(action.type) {
@@ -12,18 +9,14 @@ const blogReducer = (state, action) => {
     }
 };
 
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-    const addBlogPost = () => {
-        dispatch({ type: 'add_blogpost'});
+const addBlogPost = (dispatch) => {
+    return () => {
+    dispatch({ type: 'add_blogpost'});
     };
-
-    return (
-        <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-            {children}
-        </BlogContext.Provider>
-    );
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataCotext(
+    blogReducer, 
+    { addBlogPost},
+    []
+);
